cc99385f709ddcf3bd185f2a16f10e267a5d7ef6
electricman226 <electricman226@gmail.com>
2018-06-14 23:37:28 -0400

    Add operator concept.

---
+++ src/main/java/me/james/slavbot/commands/OperatorCommand.java
@@ -0,0 +1,29 @@
+package me.james.slavbot.commands;
+
+import com.google.gson.*;
+import java.util.*;
+import me.james.basebot.command.*;
+import me.james.slavbot.*;
+import sx.blah.discord.handle.obj.*;
+
+public abstract class OperatorCommand extends Command
+{
+    @Override
+    public String doCommand( String[] args, IUser user, IChannel chan, IMessage msg )
+    {
+        if ( !SlavBot.BOT.getConfig( chan.getGuild() ).has( "operatorChannel" ) || chan.getLongID() != SlavBot.BOT.getConfig( chan.getGuild() ).get( "operatorChannel" ).getAsLong() )
+            return "Not an operator channel.";
+        JsonArray operators = SlavBot.BOT.getConfig( chan.getGuild() ).get( "operators" ).getAsJsonArray();
+        if ( operators.size() <= 0 )
+            return "No operators? (len is 0)";
+        List< IRole > roles = user.getRolesForGuild( chan.getGuild() );
+
+        for ( JsonElement elem : operators )
+            if ( roles.stream().anyMatch( role -> role.getLongID() == elem.getAsLong() ) )
+                return doOperatorCommand( args, user, chan, msg );
+
+        return "User was not an operator.";
+    }
+
+    public abstract String doOperatorCommand( String[] args, IUser user, IChannel chan, IMessage msg );
+}
---
+++ src/main/java/me/james/slavbot/commands/operator/MuteAllCommand.java
@@ -0,0 +1,25 @@
+package me.james.slavbot.commands.operator;
+
+import java.util.*;
+import me.james.slavbot.*;
+import me.james.slavbot.commands.*;
+import sx.blah.discord.handle.obj.*;
+
+public class MuteAllCommand extends OperatorCommand
+{
+    public static HashMap< IGuild, Boolean > mutedServers = new HashMap<>();
+
+    @Override
+    public String doOperatorCommand( String[] args, IUser user, IChannel chan, IMessage msg )
+    {
+        IVoiceChannel vc;
+        if ( ( vc = user.getVoiceStateForGuild( chan.getGuild() ).getChannel() ) != null )
+            return null;
+        boolean muteStatus = !mutedServers.getOrDefault( chan.getGuild(), false );
+        for ( IUser chanUser : vc.getConnectedUsers() )
+            if ( chanUser != user && chanUser != SlavBot.BOT.getBot().getApplicationOwner() )
+                chan.getGuild().setMuteUser( chanUser, muteStatus );
+        mutedServers.put( chan.getGuild(), muteStatus );
+        return "Toggled channel mute. (" + muteStatus + ")";
+    }
+}
