2f72076e92ca1f102e5f83dc89d1cb5a686b0258
electricman226 <electricman226@gmail.com>
2018-07-30 19:55:01 -0400

    And include the template code.

---
+++ CodeTemplate.java
@@ -0,0 +1,14 @@
+import sx.blah.discord.api.*;
+import sx.blah.discord.handle.obj.*;
+import sx.blah.discord.util.*;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class %s
+{   
+    public static void run( IDiscordClient client )
+    {
+        %s
+    }
+}
